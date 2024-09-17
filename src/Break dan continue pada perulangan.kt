54fa006b1c58aaa9b4b9fe8aeeb0ade02106ed45
susi <susisalina903@gmail.com>
2024-09-17 11:28:07 +0700

    add

--- src/Break dan continue pada perulangan.kt
+++ src/Break dan continue pada perulangan.kt
@@ -3,7 +3,7 @@ fun main(args: Array<String>) {
     println("Example of Break and Continue in For-Loop")
     for(x in 1..10) {
         if(x == 7) break
-        if(x == 3) continue
+        if(x == 3) continue/////sususssssi
         print("$x ")
     }
     print('\n')
